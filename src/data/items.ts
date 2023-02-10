4434acf6775e2fa8aa4d9b05398ab12fd4e58b3f
Paulo Abdanur <ph_abdanur@yahoo.com.br>
2023-02-10 16:09:30 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2023, 0, 15),
+    category: 'food',
+    title: 'Burguer King',
+    value: 32.12,
+  },
+  {
+    date: new Date(2023, 0, 15),
+    category: 'food',
+    title: 'Outback',
+    value: 108,
+  },
+  {
+    date: new Date(2023, 0, 8),
+    category: 'rent',
+    title: 'Aluguel Apt',
+    value: 900,
+  },
+  {
+    date: new Date(2023, 1, 17),
+    category: 'salary',
+    title: 'Salario',
+    value: 4500,
+  },
+];
